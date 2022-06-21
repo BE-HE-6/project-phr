adb6542c8b0b644cbc1f8df89ceec9f87e2c1ea6
afandiaziz <afandiaziz46@gmail.com>
2022-06-21 20:29:29 +0700

    TDD controller vaccine categories method DELETE

--- spec/requests/vaccine_categories_spec.rb
+++ spec/requests/vaccine_categories_spec.rb
@@ -74,4 +74,20 @@ RSpec.describe "VaccineCategories", type: :request do
             end
         end
     end
+
+    describe 'DELETE /api/vaccine_categories/:id' do
+        context 'when the record does not exist' do
+            before { delete "/api/vaccine_categories/1000" }
+            it 'return status code ' do
+                expect(response).to have_http_status(404)
+                expect(JSON.parse(response.body)['message']).to match("Couldn't find TbVaccineCategory with 'id'=1000")
+            end
+        end
+        context 'when the record exists' do
+            before { delete "/api/vaccine_categories/#{vaccine_category_id}" }
+            it 'return status code 204' do
+                expect(response).to have_http_status(204)
+            end
+        end
+    end
 end
