90b1669f57edf0fa70bd3a490c256b703be396be
afandiaziz <afandiaziz46@gmail.com>
2022-06-21 19:45:56 +0700

    TDD model vaccine categories CREATE

--- spec/models/tb_vaccine_category_spec.rb
+++ spec/models/tb_vaccine_category_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe TbVaccineCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe '-- CREATE Vaccine Category' do
+		it 'is invalid create without a name' do
+			vaccineCategory = TbVaccineCategory.create(
+				name: nil
+			)
+			vaccineCategory.valid?
+			expect(vaccineCategory.errors[:name]).to include("can't be blank")
+		end
+		
+		it "is invalid create with a duplicate name" do
+			vaccineCategory1 = TbVaccineCategory.create(
+				name: 'Inactivated vaccines'
+			)
+			vaccineCategory2 = TbVaccineCategory.create(
+				name: 'Inactivated vaccines'
+			)
+		
+			vaccineCategory2.valid?
+			expect(vaccineCategory2.errors[:name]).to include("has already been taken")
+		end
+		
+		it 'is valid create new category' do
+			vaccineCategory = TbVaccineCategory.create(
+				name: 'Inactivated vaccines'
+			)
+			expect(vaccineCategory).to be_valid
+		end
+	end
 end
