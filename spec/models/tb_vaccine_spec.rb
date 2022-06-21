05a722a123c964230d33e9257766529dc8e0b4cc
afandiaziz <afandiaziz46@gmail.com>
2022-06-21 19:48:03 +0700

    TDD model vaccines CREATE

--- spec/models/tb_vaccine_spec.rb
+++ spec/models/tb_vaccine_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe TbVaccine, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe '-- CREATE Vaccine' do
+		it 'is invalid create with nil data' do
+			vaccine = TbVaccine.create(
+				name: nil,
+                location: nil,
+                user_id: nil,
+                vaccine_category_id: nil
+			)
+			vaccine.valid?
+			expect(vaccine.errors[:name]).to include("can't be blank")
+			expect(vaccine.errors[:location]).to include("can't be blank")
+			expect(vaccine.errors[:user_id]).to include("can't be blank")
+			expect(vaccine.errors[:user_id]).to include("is not a number")
+			expect(vaccine.errors[:tb_vaccine_category]).to include("must exist")
+			expect(vaccine.errors[:vaccine_category_id]).to include("can't be blank")
+			expect(vaccine.errors[:vaccine_category_id]).to include("is not a number")
+		end
+		
+		it "is invalid create without a valid vaccine category id" do
+			vaccineCategory = TbVaccineCategory.create(
+				name: 'Inactivated vaccines'
+			) # this id is 1
+			vaccine = TbVaccine.create(
+				name: 'Vaksin COVID-19',
+                location: 'POLRES Kota Bekasi',
+                user_id: 1,
+                vaccine_category_id: 2
+			)
+			expect(vaccine.errors[:tb_vaccine_category]).to include("must exist")
+		end
+		
+		it 'is valid create new vaccine data' do
+			vaccineCategory = TbVaccineCategory.create(
+				name: 'Inactivated vaccines'
+			)
+			vaccine = TbVaccine.create(
+				name: 'Vaksin COVID-19',
+                location: 'POLRES Kota Bekasi',
+                user_id: 1,
+                vaccine_category_id: vaccineCategory.id
+			)
+			expect(vaccineCategory).to be_valid
+			expect(vaccine).to be_valid
+		end
+	end
 end
