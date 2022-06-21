f3df6cf85754d2f8e3ae88c01c6959b7eb8a2b4b
afandiaziz <afandiaziz46@gmail.com>
2022-06-21 20:28:45 +0700

    TDD controller vaccine categories method GET by id

--- spec/requests/vaccine_categories_spec.rb
+++ spec/requests/vaccine_categories_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe "VaccineCategories", type: :request do
             expect(response).to have_http_status(200)
         end
     end
+
+    describe 'GET /api/vaccine_categories/:id' do
+        before { get "/api/vaccine_categories/#{vaccine_category_id}" }
+    
+        context 'when the record does not exist' do
+            let(:vaccine_category_id) { 10 }
+            it 'return status code 404' do
+                expect(response).to have_http_status(404)
+            end
+            it 'return message not found' do
+                expect(JSON.parse(response.body)['message']).to match("Couldn't find TbVaccineCategory with 'id'=10")
+            end
+        end
+    
+        context 'when the record exists' do
+            it 'return the category' do
+                expect(JSON.parse(response.body)).not_to be_empty
+                expect(JSON.parse(response.body)['id']).to eq(vaccine_category_id)
+            end
+        
+            it 'return status code 200' do
+                expect(response).to have_http_status(200)
+            end
+        end
+    end
 end
