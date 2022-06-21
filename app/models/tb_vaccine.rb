0633598ab3bc7fbab1a31ece77d5a24a114131ef
afandiaziz <afandiaziz46@gmail.com>
2022-06-21 19:44:25 +0700

    tb_vaccines belongs to tb_vaccine_categories

--- app/models/tb_vaccine.rb
+++ app/models/tb_vaccine.rb
@@ -1,4 +1,6 @@
 class TbVaccine < ApplicationRecord
+    belongs_to :tb_vaccine_category, foreign_key: :vaccine_category_id
+
     validates :name, presence: true
     validates :location, presence: true
     validates :user_id, presence: true, numericality: true
