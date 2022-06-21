76b3b0c2560ee3c3799cf89fd34ad32f246a18c1
afandiaziz <afandiaziz46@gmail.com>
2022-06-21 20:41:29 +0700

    request vaccines spec define factory bot

--- spec/requests/vaccines_spec.rb
+++ spec/requests/vaccines_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Vaccines", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+	let!(:vaccine_categories) { create_list(:TbVaccineCategory, 5) }
+    let!(:vaccines) { create_list(:TbVaccine, 5) }
+    let(:vaccine_id) { vaccines.first.id }
 end
