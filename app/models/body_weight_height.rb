c0f2e7e6626f8a7e3ab7182b57b2333ba8bcb757
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-06-22 18:20:11 +0700

    Add validation model BodyWeightHeight

--- app/models/body_weight_height.rb
+++ app/models/body_weight_height.rb
@@ -1,2 +1,7 @@
 class BodyWeightHeight < ApplicationRecord
+  validates :user_id, presence: true, numericality: true
+  validates :weight, presence: true, numericality: { greater_than_or_equal_to: 1}
+  validates :height, presence: true, numericality: { greater_than_or_equal_to: 1}
+  validates :date_time, presence: true
+  before_save :get_bmi
 end
