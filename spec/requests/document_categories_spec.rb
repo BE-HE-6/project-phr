1740409cb903999335625e395d13c275b27c0d36
afandiaziz <afandiaziz46@gmail.com>
2022-06-22 13:02:19 +0700

    TDD controller document categories method GET

--- spec/requests/document_categories_spec.rb
+++ spec/requests/document_categories_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe "DocumentCategories", type: :request do
 	let!(:document_categories) { create_list(:TbDocumentCategory, 5) }
     let(:document_category_id) { document_categories.first.id }
+
+    describe 'GET /api/document_categories' do
+        before { get '/api/document_categories' }
+        it 'return document categories' do
+            expect(JSON.parse(response.body)).not_to be_empty
+            expect(JSON.parse(response.body).size).to eq(5)
+        end
+        
+        it 'return status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
 end
