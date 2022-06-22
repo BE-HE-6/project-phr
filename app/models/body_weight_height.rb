9104f2e659c3c667ed26da5e4564a3ef46c5eff4
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-06-22 18:21:12 +0700

    Define method for generate BMI_Calculation

--- app/models/body_weight_height.rb
+++ app/models/body_weight_height.rb
@@ -4,4 +4,9 @@ class BodyWeightHeight < ApplicationRecord
   validates :height, presence: true, numericality: { greater_than_or_equal_to: 1}
   validates :date_time, presence: true
   before_save :get_bmi
+
+  def get_bmi
+    get_bmi = self.weight/((self.height/100)*(self.height/100))
+    return get_bmi
+  end
 end
