e007dd3659c6a6ffe511de83f2b8631c13b54eee
afandiaziz <afandiaziz46@gmail.com>
2022-06-22 12:43:21 +0700

    TDD model document categories CREATE

--- spec/models/tb_document_category_spec.rb
+++ spec/models/tb_document_category_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe TbDocumentCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe '-- CREATE Document Category' do
+		it 'is invalid create without a name' do
+			documentCategory = TbDocumentCategory.create(
+				name: nil
+			)
+			documentCategory.valid?
+			expect(documentCategory.errors[:name]).to include("can't be blank")
+		end
+		
+		it "is invalid create with a duplicate name" do
+			documentCategory1 = TbDocumentCategory.create(
+				name: 'Diagnose'
+			)
+			documentCategory2 = TbDocumentCategory.create(
+				name: 'Diagnose'
+			)
+		
+			documentCategory2.valid?
+			expect(documentCategory2.errors[:name]).to include("has already been taken")
+		end
+		
+		it 'is valid create new category' do
+			documentCategory = TbDocumentCategory.create(
+				name: 'Diagnose'
+			)
+			expect(documentCategory).to be_valid
+		end
+	end
 end
