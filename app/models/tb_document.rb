6933e4231eac2329ebda03963dfbfbbc3abc4ae4
afandiaziz <afandiaziz46@gmail.com>
2022-06-25 13:25:57 +0700

    model validation documents and belongs to document  category

--- app/models/tb_document.rb
+++ app/models/tb_document.rb
@@ -1,2 +1,8 @@
-class TbDocument < ApplicationRecord
+class TbDocument < ApplicationRecord    
+    belongs_to :tb_document_category, foreign_key: :document_category_id
+
+    validates :name, presence: true
+    validates :location, presence: true
+    validates :user_id, presence: true, numericality: true
+    validates :document_category_id, presence: true, numericality: true
 end
