e79caa097818fb788910c51a79a72eb9c2be04c1
afandiaziz <afandiaziz46@gmail.com>
2022-06-26 02:54:20 +0700

    model document mount_uploader

--- app/models/tb_document.rb
+++ app/models/tb_document.rb
@@ -1,6 +1,8 @@
 class TbDocument < ApplicationRecord    
     belongs_to :tb_document_category, foreign_key: :document_category_id
 
+    mount_uploader :doc_upload, DocUploadUploader
+
     validates :doc_name, presence: true
     validates :doc_upload, presence: true
     validates :user_id, presence: true, numericality: true
