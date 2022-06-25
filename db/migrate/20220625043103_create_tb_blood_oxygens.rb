f50fd30a3e84cdefbbca49b0f0f796f4270947b7
LuqmanRM <luqmanrofif@gmail.com>
2022-06-25 12:47:28 +0700

    migrate table tb blood oxygen

--- db/migrate/20220625043103_create_tb_blood_oxygens.rb
+++ db/migrate/20220625043103_create_tb_blood_oxygens.rb
@@ -1,6 +1,11 @@
 class CreateTbBloodOxygens < ActiveRecord::Migration[7.0]
   def change
     create_table :tb_blood_oxygens do |t|
+      t.string :blood_oxygen, null: false
+      t.string :note
+      t.datetime :date_time, null: false
+      t.integer :user_id, null: false
+      t.integer :blood_oxygen_condition_id, null: false
 
       t.timestamps
     end
