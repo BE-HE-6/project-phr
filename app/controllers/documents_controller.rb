eca8f2e147b6fba5bf99933c3ba01b0d9ca6ad9a
afandiaziz <afandiaziz46@gmail.com>
2022-06-25 13:42:20 +0700

    controller documents (index, show)

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,2 +1,10 @@
 class DocumentsController < ApplicationController
+    def index
+        @document_categories = TbDocument.withDocumentCategoryName.all
+        jsonResponse(@document_categories)
+    end
+    def show
+        @document_category = TbDocument.withDocumentCategoryName.find(params[:id])
+        jsonResponse(@document_category)
+    end
 end
