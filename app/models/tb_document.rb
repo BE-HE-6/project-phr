18eb06462aed15e1117bc04674c61b1e12283988
afandiaziz <afandiaziz46@gmail.com>
2022-06-25 13:31:51 +0700

    TDD model documents CREATE

--- app/models/tb_document.rb
+++ app/models/tb_document.rb
@@ -1,8 +1,8 @@
 class TbDocument < ApplicationRecord    
     belongs_to :tb_document_category, foreign_key: :document_category_id
 
-    validates :name, presence: true
-    validates :location, presence: true
+    validates :doc_name, presence: true
+    validates :doc_upload, presence: true
     validates :user_id, presence: true, numericality: true
     validates :document_category_id, presence: true, numericality: true
 end
--- spec/models/tb_document_spec.rb
+++ spec/models/tb_document_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe TbDocument, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe '-- CREATE Document' do
+		it 'is invalid create with nil data' do
+			document = TbDocument.create(
+				doc_name: nil,
+                doc_upload: nil,
+                user_id: nil,
+                document_category_id: nil
+			)
+			document.valid?
+			expect(document.errors[:doc_name]).to include("can't be blank")
+			expect(document.errors[:doc_upload]).to include("can't be blank")
+			expect(document.errors[:user_id]).to include("can't be blank")
+			expect(document.errors[:user_id]).to include("is not a number")
+			expect(document.errors[:tb_document_category]).to include("must exist")
+			expect(document.errors[:document_category_id]).to include("can't be blank")
+			expect(document.errors[:document_category_id]).to include("is not a number")
+		end
+		
+		it "is invalid create without a valid document category id" do
+			documentCategory = TbDocumentCategory.create(
+				name: 'Diagnose'
+			) # this id is 1
+			document = TbDocument.create(
+				doc_name: 'Diagnosa Penyakit COVID-19',
+                doc_upload: 'file.jpg',
+                user_id: 1,
+                document_category_id: 2
+			)
+			expect(document.errors[:tb_document_category]).to include("must exist")
+		end
+		
+		it 'is valid create new document data' do
+			documentCategory = TbDocumentCategory.create(
+				name: 'Diagnose'
+			)
+			document = TbDocument.create(
+				doc_name: 'Diagnosa Penyakit COVID-19',
+                doc_upload: 'file.jpg',
+                user_id: 1,
+                document_category_id: documentCategory.id
+			)
+			expect(documentCategory).to be_valid
+			expect(document).to be_valid
+		end
+	end
 end
