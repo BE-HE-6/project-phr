f0b73502a0eaf0cfa56ea2b9b4f209ca0ec0a18b
afandiaziz <afandiaziz46@gmail.com>
2022-06-25 13:20:52 +0700

    rails generate model tb_documents

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_22_053417) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_25_061947) do
   create_table "tb_document_categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tb_documents", force: :cascade do |t|
+    t.string "doc_name"
+    t.string "doc_upload"
+    t.integer "user_id"
+    t.integer "document_category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/models/tb_document_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe TbDocument, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
