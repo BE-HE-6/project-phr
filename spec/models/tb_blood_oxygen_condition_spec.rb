af0195a813fc75bf32bc7020fde917792c5680d1
LuqmanRM <luqmanrofif@gmail.com>
2022-06-25 23:01:23 +0700

    TDD insert data in tabel blood oxygen condition

--- spec/models/tb_blood_oxygen_condition_spec.rb
+++ spec/models/tb_blood_oxygen_condition_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe TbBloodOxygenCondition, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'INSERT data blood oxygen condition' do
+    it 'is invalid insert without a name' do
+			bloodOxygenConditon = TbBloodOxygenCondition.create(
+				name: nil
+			)
+			bloodOxygenConditon.valid?
+			expect(bloodOxygenConditon.errors[:name]).to include("can't be blank")
+		end
+
+    it "is invalid insert with a duplicate name" do
+			bloodOxygenConditon1 = TbBloodOxygenCondition.create(
+				name: 'high'
+			)
+			bloodOxygenConditon2 = TbBloodOxygenCondition.create(
+				name: 'high'
+			)
+		
+			bloodOxygenConditon2.valid?
+			expect(bloodOxygenConditon2.errors[:name]).to include("has already been taken")
+		end
+
+    it 'is valid create new data blood condition' do
+			bloodOxygenConditon = TbBloodOxygenCondition.create(
+				name: 'low'
+			)
+			expect(bloodOxygenConditon).to be_valid
+		end
+  end
 end
