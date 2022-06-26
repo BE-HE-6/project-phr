ac36a86690488926cb74584ec23781ce2eb3d263
afandiaziz <afandiaziz46@gmail.com>
2022-06-26 19:14:54 +0700

    document factory bot

---
+++ spec/factories/documents.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :TbDocumentCategory do
+        doc_name { Faker::Lorem.unique.word }
+        doc_upload Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/myfiles/untitled.png')))
+        user_id { Faker::Number.between(from: 1, to: 5) }
+        document_category_id { Faker::Number.between(from: 1, to: 5) }
+    end
+end
--- spec/factories/tb_documents.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :tb_document do
-    
-  end
-end
