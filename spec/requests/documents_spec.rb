f5fd6376f875e6b2cd863ac7dc4ed679168d15d2
afandiaziz <afandiaziz46@gmail.com>
2022-06-26 20:54:52 +0700

    TDD controller documents method GET

--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe "Documents", type: :request do
 	let!(:document_categories) { create_list(:TbDocumentCategory, 5) }
     let!(:documents) { create_list(:TbDocument, 5) }
     let(:document_id) { documents.first.id }
+
+    describe "GET /api/documents" do
+        before { get '/api/documents' }
+        it 'return documents' do
+            expect(JSON.parse(response.body)).not_to be_empty
+            expect(JSON.parse(response.body).size).to eq(5)
+        end
+        
+        it 'return status code 200' do
+            expect(response).to have_http_status(200)
+        end
+    end
 end
