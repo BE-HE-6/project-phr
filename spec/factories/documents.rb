9f002edb36203c1e0009320f233c61d0c4ef0ca2
afandiaziz <afandiaziz46@gmail.com>
2022-06-26 19:35:46 +0700

    update spec_helper

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
-    factory :TbDocumentCategory do
+    factory :TbDocument do
         doc_name { Faker::Lorem.unique.word }
-        doc_upload Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/myfiles/untitled.png')))
+        doc_upload { Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/myfiles/untitled.png'))) }
         user_id { Faker::Number.between(from: 1, to: 5) }
         document_category_id { Faker::Number.between(from: 1, to: 5) }
     end
