6f9249d58bb595d974b0c226174aa26450da9389
LuqmanRM <luqmanrofif@gmail.com>
2022-06-26 08:58:49 +0700

    add tdd update model tb blood oxygen condition

--- spec/models/tb_blood_oxygen_condition_spec.rb
+++ spec/models/tb_blood_oxygen_condition_spec.rb
@@ -29,4 +29,51 @@ RSpec.describe TbBloodOxygenCondition, type: :model do
 			expect(bloodOxygenConditon).to be_valid
 		end
   end
+
+  describe 'UPDATE data blood oxygen condition' do
+	it 'is invalid update without valid id' do
+		expect { 
+			bloodOxygenConditon = TbBloodOxygenCondition.find(1)
+			bloodOxygenConditon.update(
+				name: 'Inactivated vaccines'
+			) 
+		}.to raise_error(ActiveRecord::RecordNotFound)
+	end
+
+	it 'is invalid update without a name' do
+		bloodOxygenConditon = TbBloodOxygenCondition.create(
+			name: "Inactivated vaccines"
+		)
+		bloodOxygenConditon.update(
+			name: nil
+		)
+		bloodOxygenConditon.valid?
+		expect(bloodOxygenConditon.errors[:name]).to include("can't be blank")
+	end
+	
+	it "is invalid update with a duplicate name" do
+		bloodOxygenConditon1 = TbBloodOxygenCondition.create(
+			name: 'high'
+		)
+		bloodOxygenConditon2 = TbBloodOxygenCondition.create(
+			name: 'low'
+		)
+		bloodOxygenConditon2.update(
+			name: 'high'
+		)
+	
+		bloodOxygenConditon2.valid?
+		expect(bloodOxygenConditon2.errors[:name]).to include("has already been taken")
+	end
+	
+	it 'is valid update data blood oxygen condition' do
+		bloodOxygenConditon = TbBloodOxygenCondition.create(
+			name: 'low'
+		)
+		bloodOxygenConditon.update(
+			name: 'high'
+		)
+		expect(bloodOxygenConditon).to be_valid
+	end
+end
 end
