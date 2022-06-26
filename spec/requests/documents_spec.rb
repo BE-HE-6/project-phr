b848c645c384afb42266498004628c476e846cf7
afandiaziz <afandiaziz46@gmail.com>
2022-06-26 20:53:04 +0700

    request document spec define factorybot

--- spec/requests/documents_spec.rb
+++ spec/requests/documents_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Documents", type: :request do
+	let!(:document_categories) { create_list(:TbDocumentCategory, 5) }
+    let!(:documents) { create_list(:TbDocument, 5) }
+    let(:document_id) { documents.first.id }
 end
