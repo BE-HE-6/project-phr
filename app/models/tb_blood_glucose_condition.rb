bb7793a94d363f8a86262a92777698d1605829f9
LuqmanRM <luqmanrofif@gmail.com>
2022-06-26 21:07:12 +0700

    generate model tb blood glucose condition

---
+++ app/models/tb_blood_glucose_condition.rb
@@ -0,0 +1,2 @@
+class TbBloodGlucoseCondition < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_26_135910) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_26_140548) do
+  create_table "tb_blood_glucose_conditions", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tb_blood_glucose_conditions_on_name", unique: true
+  end
+
   create_table "tb_blood_glucoses", force: :cascade do |t|
     t.string "blood_glucose", null: false
     t.string "note"
