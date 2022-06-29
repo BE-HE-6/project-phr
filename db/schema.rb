923808df1ebb73b5f07eb685012753b3c49241ed
afandiaziz <afandiaziz46@gmail.com>
2022-06-29 23:06:18 +0700

    change tdd requests to controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_123540) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_25_061947) do
+  create_table "tb_document_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tb_documents", force: :cascade do |t|
+    t.string "doc_name"
+    t.string "doc_upload"
+    t.integer "user_id"
+    t.integer "document_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tb_vaccine_categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
--- spec/controllers/vaccines_spec.rb
+++ spec/controllers/vaccines_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "Vaccines", type: :request do
+RSpec.describe VaccinesController do
 	let!(:vaccine_categories) { create_list(:TbVaccineCategory, 5) }
     let!(:vaccines) { create_list(:TbVaccine, 5) }
     let(:vaccine_id) { vaccines.first.id }
 
     describe "GET /api/vaccines" do
-        before { get '/api/vaccines' }
+        before { get :index }
         it 'return vaccines' do
             expect(JSON.parse(response.body)).not_to be_empty
             expect(JSON.parse(response.body).size).to eq(5)
@@ -18,10 +18,14 @@ RSpec.describe "Vaccines", type: :request do
     end
 
     describe 'GET /api/vaccines/:id' do
-        before { get "/api/vaccines/#{vaccine_id}" }
+        before { 
+            get :show, 
+            params: {
+                id: vaccine_id
+            } 
+        }
         context 'when the record does not exist' do
             let(:vaccine_id) { 100 }
-        
             it 'return status code 404' do
                 expect(response).to have_http_status(404)
                 expect(JSON.parse(response.body)['message']).to match("Couldn't find TbVaccine with 'id'=100")
@@ -33,7 +37,6 @@ RSpec.describe "Vaccines", type: :request do
                 expect(JSON.parse(response.body)).not_to be_empty
                 expect(JSON.parse(response.body)['id']).to eq(vaccine_id)
             end
-        
             it 'return status code 200' do
                 expect(response).to have_http_status(200)
             end
@@ -43,7 +46,7 @@ RSpec.describe "Vaccines", type: :request do
     describe 'POST /api/vaccines' do
         context 'the request is invalid' do
             it "name can't be blank" do
-                post '/api/vaccines', params: { 
+                post :create, params: { 
                     name: nil,
                     location: 'Kota Bekasi',
                     user_id: 1,
@@ -53,7 +56,7 @@ RSpec.describe "Vaccines", type: :request do
                 expect(JSON.parse(response.body)['message']).to match("Validation failed: Name can't be blank")
             end
             it "location can't be blank" do
-                post '/api/vaccines', params: { 
+                post :create, params: { 
                     name: 'Inactivated vaccines',
                     location: nil,
                     user_id: 1,
@@ -63,7 +66,7 @@ RSpec.describe "Vaccines", type: :request do
                 expect(JSON.parse(response.body)['message']).to match("Validation failed: Location can't be blank")
             end
             it "user id can't be blank" do
-                post '/api/vaccines', params: { 
+                post :create, params: { 
                     name: 'Inactivated vaccines',
                     location: 'Kota Bekasi',
                     user_id: nil,
@@ -73,7 +76,7 @@ RSpec.describe "Vaccines", type: :request do
                 expect(JSON.parse(response.body)['message']).to match("Validation failed: User can't be blank, User is not a number")
             end
             it "vaccine category id can't be blank" do
-                post '/api/vaccines', params: { 
+                post :create, params: { 
                     name: 'Inactivated vaccines',
                     location: 'Kota Bekasi',
                     user_id: 1,
@@ -86,7 +89,7 @@ RSpec.describe "Vaccines", type: :request do
 
         context 'the request is valid' do
             it 'created a vaccine' do
-                post '/api/vaccines', params: {  
+                post :create, params: {  
                     name: 'Inactivated vaccines',
                     location: 'Kota Bekasi',
                     user_id: 1,
@@ -99,14 +102,24 @@ RSpec.describe "Vaccines", type: :request do
 
     describe 'DELETE /api/vaccines/:id' do
         context 'when the record does not exist' do
-            before { delete "/api/vaccines/1000" }
+            before { 
+                delete :destroy,
+                params: { 
+                    id: 1000, 
+                } 
+            }
             it 'return status code ' do
                 expect(response).to have_http_status(404)
                 expect(JSON.parse(response.body)['message']).to match("Couldn't find TbVaccine with 'id'=1000")
             end
         end
         context 'when the record exists' do
-            before { delete "/api/vaccines/#{vaccine_id}" }
+            before { 
+                delete :destroy,
+                params: { 
+                    id: vaccine_id, 
+                } 
+            }
             it 'return status code 204' do
                 expect(response).to have_http_status(204)
             end
