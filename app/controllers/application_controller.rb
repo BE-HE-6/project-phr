4dd8d504af62adcc34b3e7c0abacb2f5a6995dc1
Mohammad Yusuf <ysbcc.rys@gmail.com>
2022-06-29 23:03:15 +0700

    add authorized_user and authorize method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,15 +10,27 @@ class ApplicationController < ActionController::Base
   def decode_token
     # Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMzY5In0.5JuT54p4oa1hDNejn_CCcNI-2IbK0v4dsx32s3EWwUU
 
-    auth_header = request.headers['Authorization']
-    if auth_header
-      token = auth_header.split(' ')[1]
+    @auth_header = request.headers['Authorization']
+    if @auth_header
+      @token = @auth_header.split(' ')[1]
       begin
-        JWT.decode(token, 'secret', true, algorithm: 'HS256')
+        JWT.decode(@token, 'secret', true, algorithm: 'HS256')
       rescue JWT::DecodeError
         nil
       end
     end
+  end
+
+  def authorized_user
+    @decoded_token = decode_token
+    if @decoded_token
+      @user_id = @decoded_token[0]['user_id']
+      @user = User.find_by(id: @user_id)
+    end
+  end
+
+  def authorize
+    render json: { message: 'You have to log in' }, status: :unauthorized unless authorized_user
   end
 
   private
