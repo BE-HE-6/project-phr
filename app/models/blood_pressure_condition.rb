f8c723f4e7a4bd85b2e8aca848465dee9a91d1db
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-06-29 18:14:01 +0700

    add spec for BloodPressureCondition model and validation model

--- app/models/blood_pressure_condition.rb
+++ app/models/blood_pressure_condition.rb
@@ -1,2 +1,3 @@
 class BloodPressureCondition < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
 end
--- spec/models/blood_pressure_condition_spec.rb
+++ spec/models/blood_pressure_condition_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe BloodPressureCondition, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+    it "is valid with a name" do
+      FactoryBot.create(:blood_pressure_condition)
+    end
+
+    it "is invalid without a name" do
+      bpc = BloodPressureCondition.create(name: nil)
+      bpc.valid?
+      expect(bpc.errors[:name]).to include("can't be blank") 
+    end
+
+    it "is invalid with a duplicate name" do
+      bpc1 = FactoryBot.create(:blood_pressure_condition)
+      bpc2 = FactoryBot.build(:blood_pressure_condition)
+      bpc2.valid?
+      expect(bpc2.errors[:name]).to include("has already been taken")
+    end
+  
 end
