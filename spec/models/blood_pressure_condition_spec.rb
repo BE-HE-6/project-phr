b6ed9315ad9328eabfcb4d3f7994d99034864a45
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-06-30 18:47:55 +0700

    Complete TDD for Blood Pressure Condition model

--- spec/models/blood_pressure_condition_spec.rb
+++ spec/models/blood_pressure_condition_spec.rb
@@ -1,22 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe BloodPressureCondition, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-    it "is valid with a name" do
-      FactoryBot.create(:blood_pressure_condition)
+  
+    describe "CREATE Blood Pressure Condition" do
+      it "is valid with a name" do
+        FactoryBot.create(:blood_pressure_condition)
+      end
+  
+      it "is invalid without a name" do
+        bpc = BloodPressureCondition.create(name: nil)
+        bpc.valid?
+        expect(bpc.errors[:name]).to include("can't be blank") 
+      end
+  
+      it "is invalid with a duplicate name" do
+        bpc1 = FactoryBot.create(:blood_pressure_condition)
+        bpc2 = FactoryBot.build(:blood_pressure_condition)
+        bpc2.valid?
+        expect(bpc2.errors[:name]).to include("has already been taken")
+      end
     end
 
-    it "is invalid without a name" do
-      bpc = BloodPressureCondition.create(name: nil)
-      bpc.valid?
-      expect(bpc.errors[:name]).to include("can't be blank") 
+    describe "GET Blood Pressure Condition" do
+      it 'is valid GET with valid ID' do
+        BloodPressureCondition.create(
+          name: 'Wake Up'
+        )
+        bpc = BloodPressureCondition.find(1)
+        expect(bpc).to be_valid
+      end
+
+      it 'is invalid without valid ID' do
+        expect {BloodPressureCondition.find(1).to raise_error(ActiveRecord::RecordNotFound)}
+      end
     end
 
-    it "is invalid with a duplicate name" do
-      bpc1 = FactoryBot.create(:blood_pressure_condition)
-      bpc2 = FactoryBot.build(:blood_pressure_condition)
-      bpc2.valid?
-      expect(bpc2.errors[:name]).to include("has already been taken")
+    describe "UPDATE Blood Pressure Condition" do
+      it 'is valid with valid ID' do
+          bpc = BloodPressureCondition.create(
+            name: 'Wake Up'
+          )
+
+          bpc.update(
+            name: 'After Exercise'
+          )
+          expect(bpc).to be_valid
+      end
+
+      it 'is invalid without valid ID' do
+        expect{
+          bpc = BloodPressureCondition.find(1)
+          bpc.update(
+            name: 'Disable Condition'
+          )
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+
+      it 'is invalid with nil data' do
+        bpc = FactoryBot.create(:blood_pressure_condition)
+        bpc.update(
+          name: nil
+        )
+        bpc.valid?
+        expect(bpc.errors[:name]).to include("can't be blank")
+      end
     end
+
+    describe "DELETE Blood Pressure Condition" do
+      it 'is valid with valid ID' do
+        bpc = FactoryBot.create(:blood_pressure_condition)
+        bpc.destroy
+        expect(bpc).to be_valid
+      end
+
+      it 'is invalid without valid ID' do
+        expect {
+          BloodPressureCondition.find(1).destroy
+      }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+    
+    
   
 end
