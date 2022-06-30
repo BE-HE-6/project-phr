3dd2fc03c3062de09d5b50b38deeed7b900372e7
Belajar Git <belajargit@student.uns.ac.id>
2022-06-30 17:29:06 +0700

    done TTD model tb_lab_result

--- spec/models/tb_lab_result_spec.rb
+++ spec/models/tb_lab_result_spec.rb
@@ -2,130 +2,138 @@ require 'rails_helper'
 
 RSpec.describe TbLabResult, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  describe '-- CREATE Vaccine' do
+  describe '-- CREATE Lab Result' do
 		it 'is invalid create with nil data' do
-			vaccine = TbVaccine.create(
-				name: nil,
-                location: nil,
+			labresult = TbLabResult.create(
+				lab_name: nil,
+                lab_doc: nil,
+                lab_result_note: nil,
                 user_id: nil,
-                vaccine_category_id: nil
+                lab_result_category_id: nil
 			)
-			vaccine.valid?
-			expect(vaccine.errors[:name]).to include("can't be blank")
-			expect(vaccine.errors[:location]).to include("can't be blank")
-			expect(vaccine.errors[:user_id]).to include("can't be blank")
-			expect(vaccine.errors[:user_id]).to include("is not a number")
-			expect(vaccine.errors[:tb_vaccine_category]).to include("must exist")
-			expect(vaccine.errors[:vaccine_category_id]).to include("can't be blank")
-			expect(vaccine.errors[:vaccine_category_id]).to include("is not a number")
+			labresult.valid?
+			expect(labresult.errors[:lab_name]).to include("can't be blank")
+			expect(labresult.errors[:lab_doc]).to include("can't be blank")
+			expect(labresult.errors[:lab_result_note]).to include("can't be blank")
+			expect(labresult.errors[:user_id]).to include("is not a number")
+            expect(labresult.errors[:lab_result_category_id]).to include("is not a number")
+			expect(labresult.errors[:tb_lab_result_category]).to include("must exist")
+			expect(labresult.errors[:lab_result_category_id]).to include("can't be blank")
+			expect(labresult.errors[:lab_result_category_id]).to include("is not a number")
 		end
 		
-		it "is invalid create without a valid vaccine category id" do
-			vaccineCategory = TbVaccineCategory.create(
-				name: 'Inactivated vaccines'
-			) # this id is 1
-			vaccine = TbVaccine.create(
-				name: 'Vaksin COVID-19',
-                location: 'POLRES Kota Bekasi',
+		it "is invalid create without a valid labresult category id" do
+			labresultCategory = TbLabResultCategory.create(
+				name: 'Inactivated labresult'
+			) 
+			labresult = TbLabResult.create(
+				lab_name: 'RSUD Jakarta',
+                lab_doc: 'Upload File',
+                lab_result_note: 'Catatan Pasien',
                 user_id: 1,
-                vaccine_category_id: 2
+                lab_result_category_id: 2
 			)
-			expect(vaccine.errors[:tb_vaccine_category]).to include("must exist")
+			expect(labresult.errors[:tb_lab_result_category]).to include("must exist")
 		end
 		
-		it 'is valid create new vaccine data' do
-			vaccineCategory = TbVaccineCategory.create(
-				name: 'Inactivated vaccines'
+		it 'is valid create new labresult data' do
+			labresultCategory = TbLabResultCategory.create(
+				name: 'Inactivated labresult'
 			)
-			vaccine = TbVaccine.create(
-				name: 'Vaksin COVID-19',
-                location: 'POLRES Kota Bekasi',
+			labresult = TbLabResult.create(
+				lab_name: 'RSUD Jakarta',
+                lab_doc: 'Upload File',
+                lab_result_note: 'Catatan Pasien',
                 user_id: 1,
-                vaccine_category_id: vaccineCategory.id
+                lab_result_category_id: labresultCategory.id
 			)
-			expect(vaccineCategory).to be_valid
-			expect(vaccine).to be_valid
+			expect(labresultCategory).to be_valid
+			expect(labresult).to be_valid
 		end
 	end
-    describe '-- UPDATE Vaccine' do
+    describe '-- UPDATE Lab Result' do
         it 'is invalid update without valid id' do
             expect { 
-                vaccine = TbVaccine.find(1)
-                vaccine.update(
-                    name: 'Vaksin COVID-19'
+                labresult = TbLabResult.find(1)
+                labresult.update(
+                    lab_name: 'RSUD Jakarta'
                 ) 
             }.to raise_error(ActiveRecord::RecordNotFound)
         end
         it 'is invalid update without a name' do
-			vaccineCategory = TbVaccineCategory.create(
-				name: 'Inactivated vaccines'
+			labresultCategory = TbLabResultCategory.create(
+				name: 'Inactivated labresult'
 			)
-			vaccine = TbVaccine.create(
-				name: 'Vaksin COVID-19',
-                location: 'POLRES Kota Bekasi',
+			labresult = TbLabResult.create(
+				lab_name: 'RSUD Jakarta',
+                lab_doc: 'Upload File',
+                lab_result_note: 'Catatan Pasien',
                 user_id: 1,
-                vaccine_category_id: vaccineCategory.id
+                lab_result_category_id: labresultCategory.id
 			)
-            vaccine.update(
-                name: nil
+            labresult.update(
+                lab_name: nil
             )
-            vaccine.valid?
-            expect(vaccine.errors[:name]).to include("can't be blank")
+            labresult.valid?
+            expect(labresult.errors[:lab_name]).to include("can't be blank")
         end
         
-        it 'is valid update Vaccine' do
-			vaccineCategory = TbVaccineCategory.create(
-				name: 'Inactivated vaccines'
+        it 'is valid update Lab Result' do
+			labresultCategory = TbLabResultCategory.create(
+				name: 'Inactivated labresult'
 			)
-			vaccine = TbVaccine.create(
-				name: 'Vaksin COVID-19',
-                location: 'POLRES Kota Bekasi',
+			labresult = TbLabResult.create(
+				lab_name: 'RSUD Jakarta',
+                lab_doc: 'Upload File',
+                lab_result_note: 'Catatan Pasien',
                 user_id: 1,
-                vaccine_category_id: vaccineCategory.id
+                lab_result_category_id: labresultCategory.id
 			)
-            vaccine.update(
-                location: 'POLRES Kota Jakarta',
+            labresult.update(
+                lab_name: 'RSUD Jakarta',
             )
-            expect(vaccine).to be_valid
+            expect(labresult).to be_valid
         end
     end
-    describe '-- DELETE Vaccine' do
+    describe '-- DELETE Lab Result' do
         it 'is invalid delete without valid id' do
-            expect { TbVaccine.destroy(1) }.to raise_error(ActiveRecord::RecordNotFound)
+            expect { TbLabResult.destroy(1) }.to raise_error(ActiveRecord::RecordNotFound)
         end
         
-        it 'is valid delete Vaccine' do
-			vaccineCategory = TbVaccineCategory.create(
-				name: 'Inactivated vaccines'
+        it 'is valid delete Lab Result' do
+			labresultCategory = TbLabResultCategory.create(
+				name: 'Inactivated labresult'
 			)
-			vaccine = TbVaccine.create(
-				name: 'Vaksin COVID-19',
-                location: 'POLRES Kota Bekasi',
+			labresult = TbLabResult.create(
+				lab_name: 'RSUD Jakarta',
+                lab_doc: 'Upload File',
+                lab_result_note: 'Catatan Pasien',
                 user_id: 1,
-                vaccine_category_id: vaccineCategory.id
+                lab_result_category_id: labresultCategory.id
 			)
-            vaccine.destroy
-            expect(vaccine).to be_valid
+            labresult.destroy
+            expect(labresult).to be_valid
         end
     end
 
-    describe '-- GET Vaccine' do
+    describe '-- GET Lab Result' do
         it 'is invalid get without valid id' do
-            expect { TbVaccine.find(1) }.to raise_error(ActiveRecord::RecordNotFound)
+            expect { TbLabResult.find(1) }.to raise_error(ActiveRecord::RecordNotFound)
         end
 
-        it 'is valid get Vaccine with valid id' do
-			vaccineCategory = TbVaccineCategory.create(
-				name: 'Inactivated vaccines'
+        it 'is valid get Lab Result with valid id' do
+			labresultCategory = TbLabResultCategory.create(
+				name: 'Inactivated labresult'
 			)
-			TbVaccine.create(
-				name: 'Vaksin COVID-19',
-                location: 'POLRES Kota Bekasi',
+			TbLabResult.create(
+				lab_name: 'RSUD Jakarta',
+                lab_doc: 'Upload File',
+                lab_result_note: 'Catatan Pasien',
                 user_id: 1,
-                vaccine_category_id: vaccineCategory.id
+                lab_result_category_id: labresultCategory.id
 			)
-            vaccine = TbVaccine.find(1)
-            expect(vaccine).to be_valid
+            labresult = TbLabResult.find(1)
+            expect(labresult).to be_valid
         end
     end
 end
