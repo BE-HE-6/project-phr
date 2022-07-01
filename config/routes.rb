9acf361359b926477ddfd8ea94cba6e82aa9fd47
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-07-01 09:43:52 +0700

    Define routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '/api' do
+    resources :blood_pressure, only: [:index, :show, :create, :destroy]
+    resources :blood_pressure_condition, only: [:index, :create, :update, :show, :destroy ]
+  end 
 end
