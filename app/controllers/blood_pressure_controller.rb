59562065a0ea28dbc02175540611d930405eb516
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-07-02 12:24:25 +0700

    Define TDD and controller for BloodPressure

--- app/controllers/blood_pressure_controller.rb
+++ app/controllers/blood_pressure_controller.rb
@@ -1,2 +1,28 @@
 class BloodPressureController < ApplicationController
+
+    
+    def index
+        @bp = BloodPressure.all
+        jsonResponse(@bp)
+    end
+
+    def show
+        @bp = BloodPressure.find(params[:id])
+        jsonResponse(@bp)
+    end
+
+    def create
+        @bp = BloodPressure.create!(bp_params)
+        jsonResponse(@bp, :created)
+    end
+
+    def destroy
+        @bp = BloodPressure.find(params[:id]).destroy!
+        head :no_content
+    end
+
+    private
+    def bp_params
+        params.permit(:user_id, :blood_pressure_condition_id, :sistole, :diastole, :pulse, :note, :date_time)
+    end
 end
