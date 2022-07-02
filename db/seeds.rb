489be5d1faaebf23890fd6786db89008f2e6b1de
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-07-02 22:40:13 +0700

    Define database seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+    # 20.times do |id|
+    #     BodyWeightHeight.create!(
+    #         user_id: Faker::Number.between(from: 1, to: 4),
+    #         weight: Faker::Number.decimal(l_digits: 2),
+    #         height: Faker::Number.decimal(l_digits: 2),
+    #         note: Faker::Lorem.sentence(word_count: 3),
+    #         bmi_calculation: Faker::Number.decimal(l_digits: 2),
+    #         date_time: "2022-06-22 13:50:19"
+    #     )
+    # end
+
+    # puts "Created #{BodyWeightHeight.count} movies"
