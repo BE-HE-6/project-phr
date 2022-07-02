627d0cf8241500ea268a7e5f05c7561a1850fba8
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2022-07-02 22:39:34 +0700

    Define factory faker for BodyWeightHeight

--- spec/factories/body_weight_heights.rb
+++ spec/factories/body_weight_heights.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :body_weight_height do
-    user_id { 1 }
-    weight { 1.5 }
-    height { 1.5 }
-    note { "MyText" }
-    bmi_calculation { 1.5 }
+    user_id { Faker::Number.between(from: 1, to: 4) }
+    weight { Faker::Number.decimal(l_digits: 2) }
+    height { Faker::Number.decimal(l_digits: 2) }
+    note { Faker::Lorem.sentence(word_count: 3) }
+    bmi_calculation { Faker::Number.decimal(l_digits: 2) }
     date_time { "2022-06-22 13:50:19" }
   end
 end
