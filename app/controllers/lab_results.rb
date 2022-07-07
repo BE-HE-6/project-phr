618d13fafb065609f501fb5ccb7473f230fb42fb
afandiaziz <afandiaziz46@gmail.com>
2022-07-07 15:02:29 +0700

    TDD Controller lab measurement, lab result categories

--- app/controllers/lab_results.rb
+++ app/controllers/lab_results.rb
@@ -2,20 +2,20 @@ class LabResultsController < ApplicationController
     before_action :authorize, :check_role_user
     
     def index
-        lab_results = TbLabResult.all
-        jsonResponse(lab_results)
+        @lab_results = TbLabResult.all
+        jsonResponse(@lab_results)
     end
     def show
-        lab_result = TbLabResult.find(params[:id])
-        jsonResponse(lab_result)
+        @lab_result = TbLabResult.find(params[:id])
+        jsonResponse(@lab_result)
     end
     def create
-        lab_result = TbLabResult.create!(lab_result_params)
-        jsonResponse(lab_result, :created)
+        @lab_result = TbLabResult.create!(lab_result_params)
+        jsonResponse(@lab_result, :created)
     end
     def update
-        lab_result = TbLabResult.find(params[:id])
-        jsonResponse(lab_result.update!(lab_result_params), :no_content)
+        @lab_result = TbLabResult.find(params[:id])
+        jsonResponse(@lab_result.update!(lab_result_params), :no_content)
     end
     def destroy
         TbLabResult.destroy(params[:id])
