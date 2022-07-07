77e7769f1a2f5153bc8197a1eaa8d255b408c5b7
taufiqkba <taufiqkurniawan9991@gmail.com>
2022-07-07 16:16:37 +0700

    Add TDD LabResult controller

--- app/controllers/lab_results_controller.rb
+++ app/controllers/lab_results_controller.rb
@@ -5,18 +5,22 @@ class LabResultsController < ApplicationController
         @lab_results = TbLabResult.all
         jsonResponse(@lab_results)
     end
+
     def show
         @lab_result = TbLabResult.find(params[:id])
         jsonResponse(@lab_result)
     end
+
     def create
         @lab_result = TbLabResult.create!(lab_result_params)
         jsonResponse(@lab_result, :created)
     end
+
     def update
         @lab_result = TbLabResult.find(params[:id])
         jsonResponse(@lab_result.update!(lab_result_params), :no_content)
     end
+    
     def destroy
         TbLabResult.destroy(params[:id])
         head :no_content
